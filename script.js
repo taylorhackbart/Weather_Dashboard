62a6ee59f2ecf800375c426fbdce94e0a99118ef
Taylor <taylor.hackbart@du.edu>
2020-10-07 08:34:19 -0600

    changed images to texts

--- script.js
+++ script.js
@@ -90,15 +90,15 @@ function CitySearch() {
       
      //creating uv values to append specific uv levels 
       if(response.value < 2){
-        uvTag.append($("#img1").css("display","block"))
+        uvTag.addClass((uvTag).css("text-color", "green"))
       } else if (response.value < 5 && response.value>2 ){
-        uvTag.append($("#img2").css("display","block"))
+        uvTag.addClass((uvTag).css("text-color","yellow"))
       } else if(response.value < 7 && response.value>5){
-        uvTag.append($("#img3").css("display","block"))
+        uvTag.addClass((uvTag).css("background-color","orange"))
       } else if (response.value < 10 && response.value>7){
-        uvTag.append($("#img4").css("display","block"))
+        uvTag.addClass((uvTag).css("background-color","red"))
       } else {
-        uvTag.append($("#img5").css("display","block"))
+        uvTag.addClass((uvTag).css("background-color","pink"))
       }      
       first.append(uvTag);
     });
